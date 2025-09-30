23966dbf2a76cd2c8e5588f7694607eb7a6421bc
Gabriel Goxhaj <gabrielgoxhaj2013@hotmail.it>
2025-10-01 00:07:42 +0200

    first commit

---
+++ BLogic/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartAccademy8.BLogic
+{
+    internal static class FirstDay
+    {
+
+        private static int x;
+        internal static void Welcome()
+        {
+            Console.WriteLine("Hello, Betacom!");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Cambio colore");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Scrivi testo: ");
+
+            string inputConsole = "";
+
+            inputConsole = Console.ReadLine();
+            Console.WriteLine(inputConsole.ToUpper());
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Numero: " + i);
+                Console.WriteLine($"Numero: {i}");
+            }
+
+            string x = "";
+            string y = null;
+
+            bool status = true;
+            while (true)
+            {
+                Console.WriteLine("Sono in un ciclo while");
+                if (Console.ReadLine().ToUpper() == "F")
+                { break; }
+                else
+                { Console.WriteLine("Ciclo in continuazione"); }
+            }
+
+            do
+            {
+                Console.WriteLine(status);
+                status = false;
+            } while (status); // continua ad andare avanti finchè status = true, esce subito
+
+            string sName = "Claudio";
+            foreach (char c in sName)
+            {
+                Console.Write(c.ToString().ToUpper() + ',');
+            }
+        }
+
+        internal static void RunMultiply()
+        {
+            string rowSeparator = new string('-', 100);
+            int n1 = 0;
+            int n2 = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(rowSeparator);
+                Console.WriteLine("Semplice moltiplicatore di 2 numeri");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                
+                Console.Write("Inserire Numero 1: ");
+                if (!int.TryParse(Console.ReadLine(), out n1))
+                {
+                    Console.Write("Numero non valido! Premi un tasto per continuare ");
+                    Console.ReadLine();
+                }
+                Console.Write("Inserire Numero 2: ");
+                if (!int.TryParse(Console.ReadLine(), out n2))
+                {
+                    Console.Write("Numero non valido! Premi un tasto per continuare ");
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine("Il risultato della moltiplicazione tra i numeri " + n1 + " e " + n2 + " è pari a " + ComputeMultiply(n1, n2));
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(rowSeparator);
+                Console.WriteLine("Vuoi continuare? (S/N) ? ");
+                Console.ForegroundColor = ConsoleColor.White;
+                if (Console.ReadLine().Equals("N", StringComparison.CurrentCultureIgnoreCase))
+                { break; }
+            }
+        }
+
+        private static int ComputeMultiply(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic;
+using StartAccademy8.BLogic;
+using System.Linq.Expressions;
+
+namespace StartAccademy8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // FirstDay.Welcome();
+
+            FirstDay.RunMultiply();
+
+            //try
+            //{
+            //    Console.Write("Inserire Numero 1: ");
+            //    n1 = Convert.ToInt16(Console.ReadLine());
+            //    Console.Write("Inserire Numero 2: ");
+            //    n2 = Convert.ToInt16(Console.ReadLine());
+            //    Console.WriteLine(FirstDay.ComputeSquare(n1, n2));
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"E' stato inserito un numero non valido. Descrizione Errore {ex.Message}");
+            //    Console.WriteLine("Operazione annullata, programma chiuso");
+            //    return;
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Eseguito finally");
+            //}
+        }
+
+    }
+}
